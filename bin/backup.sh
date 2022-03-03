c20a7d82a38743cc289380ab655431761e537417
akuballa <akuballa@users.noreply.github.com>
2022-03-03 23:26:28 +0100

    Update backup.sh

--- bin/backup.sh
+++ bin/backup.sh
@@ -3,18 +3,33 @@ SERVER=""
 USER=""
 PASSWORD=""
 TARGETS=(
-"/mnt/backup/01/" 
-"/mnt/backup/02/"
+"/mnt/backup/01/data/" 
+"/mnt/backup/02/data/"
 )
 NOW=$(date +"%Y%m%d%H%M%S")
 
-# install sshpass
+# sshpass
 if ! [ -x "$(command -v sshpass)" ]; then
-	apt -y install sshpass
-fi	
+	echo "Error: sshpass missing. Please run: sudo apt -y install sshpass"
+	exit
+fi
+# rsync
+if ! [ -x "$(command -v rsync)" ]; then
+	echo "Error: rsync missing. Please run: sudo apt -y install rsync"
+	exit
+fi
 
 for ix in ${!TARGETS[*]}
 do
+	if ! [ -d "${TARGETS[$ix]}" ]; then
+		mkdir ${TARGETS[$ix]}
+	fi
+	if ! [ -f "${TARGETS[$ix]}../log.txt" ]; then
+		touch "${TARGETS[$ix]}../log.txt"
+		sleep 1
+		NOW=$(date +"%Y%m%d%H%M%S")
+	fi
+
 	DATE=$(date -r "${TARGETS[$ix]}../log.txt" +"%Y%m%d%H%M%S")
 	if [ "$DATE" -lt "$NOW" ]; then
 		NOW="$DATE"
@@ -22,7 +37,7 @@ do
 	fi
 done
 
-echo "$TARGET"
+echo "Target: $TARGET"
 if [ -v TARGET ]; then
-	sshpass -p "$PASSWORD" rsync --delete --stats --exclude 'logs' -av $USER@$SERVER:/var/www/ "$TARGET" 3>&1 1>"$TARGET../log.txt" 2>&1
+	sshpass -p "$PASSWORD" rsync --delete --stats --exclude={'class','logs'} -av $USER@$SERVER:/var/www/ "$TARGET" 3>&1 1>"$TARGET../log.txt" 2>&1
 fi
